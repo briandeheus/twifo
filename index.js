15844341ad7b491c2ba598e221226e272834e63e
Brian de Heus <brian@devel.2connect.jp>
2014-11-24 20:34:15 +0900

    This is awesome

--- index.js
+++ index.js
@@ -5,16 +5,46 @@ var then   = Date.now();
 //Load configuration file
 carton.cfg.load('./cfg/' + process.env.NODE_ENV + '.json');
 carton.setAppDir(appDir);
+carton.add(require('carton-snor'));
+carton.add(require('./modules/error'));
 
 //Load modules
 carton.add(require('./modules/logger'));
 carton.add(require('./modules/server'));
-carton.add(require('carton-snor'));
+carton.add(require('./modules/mysql'));
+carton.add(require('./modules/twitter'));
+carton.add(require('./modules/user'));
+carton.add(require('./modules/topic'));
+carton.add(require('./modules/thread'));
+carton.add(require('./modules/post'));
+
 
 carton.setup(function (error) {
 
 	carton.logger.info('Setting up controllers');
+
+	carton.server.methods.use(function(req, res, next) {
+		
+		var then = Date.now();
+		res.on('finish', function () {
+
+			carton.logger.verbose('Response finished in', Date.now() - then, 'ms');
+
+		});
+
+		next();
+
+	});	
+
 	require('./controllers/home');
+	require('./controllers/login/twitter');
+	
+	require('./controllers/thread/create');
+	require('./controllers/thread/view');
+
+	require('./controllers/post/add');
+
 	carton.logger.info('Carton started up in', Date.now() - then, 'ms');
+
 
 });
--- modules/server/index.js
+++ modules/server/index.js
@@ -1,7 +1,10 @@
-var carton = require('carton');
-var app    = require('express')();
-var port   = carton.cfg.get('server.port');
-var secret = carton.cfg.get('server.secret');
+var carton     = require('carton');
+var app        = require('express')();
+var port       = carton.cfg.get('server.port');
+var secret     = carton.cfg.get('server.secret');
+var session    = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
 var server;
 
 //Allow for nginx proxying.
@@ -10,7 +13,13 @@ app.enable('trust proxy');
 //Adding middleware.
 app.use(require('body-parser')());
 app.use(require('cookie-parser')());
-app.use(require('express-session')({secret: secret}));
+
+app.use(session({
+    store: new RedisStore({host: carton.cfg.get('redis.host')}),
+    secret: secret
+}));
+
+var session = 
 
 exports.methods = app;
 exports.name    = 'server';
